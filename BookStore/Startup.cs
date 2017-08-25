11b6f46c84cbffd11230a98a82279f96b47b195e
James Parry-Turner <jamesparryturner@gmail.com>
2017-08-25 22:35:54 +0100

    Implemented an admin user and allow onlu certain actions for that user to be able to execute Changed the login check to allow the manual creation of the Admin User in the Startup.cs The User is being created with the Identity Framework that is offered by .NET to ensure all that hashing/salting is professional standard

--- BookStore/Startup.cs
+++ BookStore/Startup.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Owin;
 using Owin;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
 
 [assembly: OwinStartupAttribute(typeof(BookStore.Startup))]
 namespace BookStore
@@ -9,6 +11,33 @@ namespace BookStore
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            create();
+        }
+
+        public void create()
+        {
+
+            BookStore.Models.ApplicationDbContext context = new Models.ApplicationDbContext();
+
+            var roleManger = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserManager = new UserManager<Models.ApplicationUser>(new UserStore<Models.ApplicationUser>(context));
+
+            var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+            role.Name = "Admin";
+            roleManger.Create(role);
+
+            var user = new Models.ApplicationUser();
+            user.Email = "testjames@email.com";
+            user.UserName = user.Email;
+
+            string userPassword = "Pass@Word1";
+
+            var chkUser = UserManager.Create(user, userPassword);
+
+            if (chkUser.Succeeded)
+            {
+                var result = UserManager.AddToRole(user.Id, "Admin");
+            }
         }
     }
 }
